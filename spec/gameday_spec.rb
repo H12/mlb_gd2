24e69117080da8a663507349636acd6ddae5ca24
Henry Firth <henrynf12@gmail.com>
2015-10-01 14:29:35 -0500

    Add gameday spec files

---
+++ spec/gameday_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+
+require_relative "../lib/gameday"
+
+describe "Gameday" do
+
+  let(:gameday) { Gameday.new("2014", "06", "08") }
+
+  describe "#games" do
+    it "returns an array of Game objects which take place on the current Gameday" do
+      expect(gameday.games.sample).to be_a(Game)
+    end
+  end
+
+  describe "#date" do
+    it "returns the date associated with the current Gameday object" do
+      expect(gameday.date).to eq("June 8, 2014")
+    end
+  end
+
+end
