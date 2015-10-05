6f07a92f5c41872ee3a933a98e88013f6e26caa8
Henry Firth <henrynf12@gmail.com>
2015-10-05 13:44:38 -0500

    Add Game#linescores along with corresponding test

--- lib/game.rb
+++ lib/game.rb
@@ -14,6 +14,25 @@ class Game
     ]
   end
 
+  def linescores
+    linescore = boxscore.xpath("//linescore").first
+
+    Hash[
+      :away, {
+        inning_line_score: boxscore.xpath("//inning_line_score").map { |inning| inning["away"].to_i },
+        runs: linescore["away_team_runs"].to_i,
+        hits: linescore["away_team_hits"].to_i,
+        errors: linescore["away_team_errors"].to_i
+      },
+      :home, {
+        inning_line_score: boxscore.xpath("//inning_line_score").map { |inning| inning["home"].to_i },
+        runs: linescore["home_team_runs"].to_i,
+        hits: linescore["home_team_hits"].to_i,
+        errors: linescore["home_team_errors"].to_i
+      }
+    ]
+  end
+
 private
 
 attr_reader :url, :boxscore
