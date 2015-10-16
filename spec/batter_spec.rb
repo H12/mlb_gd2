ab97ac7139f0a494dd0f4ce295cff88a91cfb8aa
Henry Firth <henrynf12@gmail.com>
2015-10-16 14:49:53 -0500

    Include helpers in batter_spec

--- spec/batter_spec.rb
+++ spec/batter_spec.rb
@@ -4,6 +4,8 @@ require_relative "../lib/mlb_gd2/batter"
 
 describe "Batter" do
 
+  include Helpers
+
   let(:batter) { Batter.new(generate_nokogiri_xml("http://gd2.mlb.com/components/game/mlb/year_2015/month_10/day_13/gid_2015_10_13_slnmlb_chnmlb_1/boxscore.xml").xpath("//batter").first) }
 
   describe "#name" do
