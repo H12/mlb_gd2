2c5dc23586f898a45b21e9285b3a3ebe00c14a3c
Henry Firth <henrynf12@gmail.com>
2015-10-16 12:19:59 -0500

    Add Batter class with #name and #position attrs and tests

--- lib/mlb_gd2/batter.rb
+++ lib/mlb_gd2/batter.rb
@@ -1,3 +1,16 @@
+require "net/http"
+require "nokogiri"
+require_relative "./helpers"
+
 class Batter
+
+  attr_reader :name, :position
+
+  include Helpers
+
+  def initialize(batter_xml)
+    @name = batter_xml.attribute("name_display_first_last").value
+    @position = batter_xml.attribute("pos").value
+  end
 
 end
--- spec/batter_spec.rb
+++ spec/batter_spec.rb
@@ -1,10 +1,22 @@
 require "rspec"
 
-require_relative "../lib/mlb_gd2/player"
+require_relative "../lib/mlb_gd2/batter"
 
 describe "Batter" do
 
-  let(:player) { Batter.new(batter_xml) }
+  let(:batter) { Batter.new(generate_nokogiri_xml("http://gd2.mlb.com/components/game/mlb/year_2015/month_10/day_13/gid_2015_10_13_slnmlb_chnmlb_1/boxscore.xml").xpath("//batter").first) }
+
+  describe "#name" do
+    it "returns a batter's full name" do
+      expect(batter.name).to eq("Dexter Fowler")
+    end
+  end
+
+  describe "#position" do
+    it "returns a batter's position" do
+      expect(batter.position).to eq("CF")
+    end
+  end
 
 
 
