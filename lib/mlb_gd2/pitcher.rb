dfc8766d4974104273acba97e9b6ae24f984f702
Henry Firth <henrynf12@gmail.com>
2015-10-17 16:29:53 -0500

    Add stats for pitchers, along with corresponding tests

--- lib/mlb_gd2/pitcher.rb
+++ lib/mlb_gd2/pitcher.rb
@@ -4,12 +4,24 @@ require_relative "./helpers"
 
 class Pitcher
 
-  attr_reader :name, :position
+  attr_reader :name, :outs, :walks, :strikeouts, :hits, :runs, :homeruns, :earned_runs, :era
 
   include Helpers
 
   def initialize(pitcher_xml)
     @name = pitcher_xml.attribute("name").value
+    @outs = pitcher_xml.attribute("out").value
+    @walks = pitcher_xml.attribute("bb").value
+    @strikeouts = pitcher_xml.attribute("so").value
+    @hits = pitcher_xml.attribute("h").value
+    @runs = pitcher_xml.attribute("r").value
+    @homeruns = pitcher_xml.attribute("hr").value
+    @earned_runs = pitcher_xml.attribute("er").value
+    @era  = pitcher_xml.attribute("era").value
+  end
+
+  def ip
+    "#{(outs.to_i / 3).to_s}.#{outs.to_i%3}"
   end
 
 end
--- spec/pitcher_spec.rb
+++ spec/pitcher_spec.rb
@@ -14,4 +14,58 @@ describe "Pitcher" do
     end
   end
 
+  describe "#outs" do
+    it "returns a pitchers outs" do
+      expect(pitcher.outs).to eq("9")
+    end
+  end
+
+  describe "#walks" do
+    it "returns a pitchers walks" do
+      expect(pitcher.walks).to eq("1")
+    end
+  end
+
+  describe "#strikeouts" do
+    it "returns a pitchers strikeouts" do
+      expect(pitcher.strikeouts).to eq("5")
+    end
+  end
+
+  describe "#hits" do
+    it "returns a pitchers hits" do
+      expect(pitcher.hits).to eq("4")
+    end
+  end
+
+  describe "#runs" do
+    it "returns a pitchers runs" do
+      expect(pitcher.runs).to eq("4")
+    end
+  end
+
+  describe "#homeruns" do
+    it "returns a pitchers homeruns" do
+      expect(pitcher.homeruns).to eq("1")
+    end
+  end
+
+  describe "#earned_runs" do
+    it "returns a pitchers earned_runs" do
+      expect(pitcher.earned_runs).to eq("4")
+    end
+  end
+
+  describe "#era" do
+    it "returns a pitchers era" do
+      expect(pitcher.era).to eq("3.48")
+    end
+  end
+
+  describe "#ip" do
+    it "returns a pitchers ip" do
+      expect(pitcher.ip).to eq("3.0")
+    end
+  end
+
 end
