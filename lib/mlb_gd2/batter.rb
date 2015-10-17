7e610b9ebd892a1e597d47bb8dcd851d1e4b095d
Henry Firth <henrynf12@gmail.com>
2015-10-17 15:03:45 -0500

    Add batter statistics and corresponding tests

--- lib/mlb_gd2/batter.rb
+++ lib/mlb_gd2/batter.rb
@@ -4,13 +4,26 @@ require_relative "./helpers"
 
 class Batter
 
-  attr_reader :name, :position
+  attr_reader :name, :position, :at_bats, :runs, :hits, :doubles, :triples, :homeruns, :rbis, :walks, :strikeouts, :avg, :obp, :slg, :ops
 
   include Helpers
 
   def initialize(batter_xml)
-    @name = batter_xml.attribute("name").value
-    @position = batter_xml.attribute("pos").value
+    @name       = batter_xml.attribute("name").value
+    @position   = batter_xml.attribute("pos").value
+    @at_bats    = batter_xml.attribute("ab").value
+    @runs       = batter_xml.attribute("r").value
+    @hits       = batter_xml.attribute("h").value
+    @doubles    = batter_xml.attribute("d").value
+    @triples    = batter_xml.attribute("t").value
+    @homeruns   = batter_xml.attribute("hr").value
+    @rbis       = batter_xml.attribute("rbi").value
+    @walks      = batter_xml.attribute("bb").value
+    @strikeouts = batter_xml.attribute("so").value
+    @avg        = batter_xml.attribute("avg").value
+    @obp        = batter_xml.attribute("obp").value
+    @slg        = batter_xml.attribute("slg").value
+    @ops        = batter_xml.attribute("ops").value
   end
 
 end
--- spec/batter_spec.rb
+++ spec/batter_spec.rb
@@ -20,4 +20,82 @@ describe "Batter" do
     end
   end
 
+  describe "#at_bats" do
+    it "returns a batter's at_bats" do
+      expect(batter.at_bats).to eq("3")
+    end
+  end
+
+  describe "#runs" do
+    it "returns a batter's runs" do
+      expect(batter.runs).to eq("0")
+    end
+  end
+
+  describe "#hits" do
+    it "returns a batter's hits" do
+      expect(batter.hits).to eq("0")
+    end
+  end
+
+  describe "#doubles" do
+    it "returns a batter's doubles" do
+      expect(batter.doubles).to eq("0")
+    end
+  end
+
+  describe "#triples" do
+    it "returns a batter's triples" do
+      expect(batter.triples).to eq("0")
+    end
+  end
+
+  describe "#homeruns" do
+    it "returns a batter's homeruns" do
+      expect(batter.homeruns).to eq("0")
+    end
+  end
+
+  describe "#rbis" do
+    it "returns a batter's rbis" do
+      expect(batter.rbis).to eq("0")
+    end
+  end
+
+  describe "#walks" do
+    it "returns a batter's walks" do
+      expect(batter.walks).to eq("1")
+    end
+  end
+
+  describe "#strikeouts" do
+    it "returns a batter's strikeouts" do
+      expect(batter.strikeouts).to eq("1")
+    end
+  end
+
+  describe "#avg" do
+    it "returns a batter's avg" do
+      expect(batter.avg).to eq(".188")
+    end
+  end
+
+  describe "#obp" do
+    it "returns a batter's obp" do
+      expect(batter.obp).to eq(".235")
+    end
+  end
+
+  describe "#slg" do
+    it "returns a batter's slg" do
+      expect(batter.slg).to eq(".438")
+    end
+  end
+
+  describe "#ops" do
+    it "returns a batter's ops" do
+      expect(batter.ops).to eq(".673")
+    end
+  end
+
 end
